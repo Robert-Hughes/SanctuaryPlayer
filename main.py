1a1bc8561e682d1eb50a3e2ad85545351516cb56
Rob Hughes <xman256@gmail.com>
2022-06-22 19:30:48 +0100

    Add Google Cloud Datastore

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # Note that the name of this file is important, as Google App Engine will look for a file called main.py
 # containing a WSGI-compatible object by default - https://cloud.google.com/appengine/docs/standard/python3/runtime#application_startup
 
-from flask import Flask
+from flask import Flask, request
+from google.cloud import datastore
+
 app = Flask(__name__)
 
-@app.route("/")
+datastore_client = datastore.Client()
+
+@app.route("/save-position", methods=['POST'])
 def test():
-    return "Hello test!"
+    return "Hello test!" + str(request.args)
 
 # Note that static files are not served from Flask, as that isn't recommended: 
 # Docs: https://flask.palletsprojects.com/en/2.1.x/quickstart/#static-files
