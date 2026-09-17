5804c9c07f162d83556e19e1ff3528132d130db2
Rob Hughes <xman256@gmail.com>
2026-09-17 22:38:32 +0100

    Windows subsystem

--- native/desktop/src/main.rs
+++ native/desktop/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+
 use sanctuary_player_app::playback::DecodeMode;
 use sanctuary_player_app::video::VideoSource;
 use sanctuary_player_app::{AppEvent, SanctuaryPlayerApp};
